c5150fa1cedd0b61589b1f106e8d48ee3b2e6a7c
suzuka-fukami <suzuka.fukami@gmail.com>
2021-07-13 14:10:46 +1000

    Passing category id that was pressed on CategoriesScreen to CategoryMealsScreen, and display the correct category

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -17,7 +17,10 @@ export const CategoriesScreen = (props) => {
       <TouchableOpacity
         style={styles.gridItem}
         onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: { categoryId: itemData.item.id },
+          });
         }}
       >
         <View>
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Platform } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 export const CategoryMealsScreen = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>Category Meal Screen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Meal Detail!"
         onPress={() => {
@@ -21,6 +27,19 @@ export const CategoryMealsScreen = (props) => {
       />
     </View>
   );
+};
+
+//navigationOptions is a property and a function, that takes navigation data as a property.
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+  };
 };
 
 const styles = StyleSheet.create({
