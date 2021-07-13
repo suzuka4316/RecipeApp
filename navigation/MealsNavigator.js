22f9e43e48ef3a9f5a01ede4d03182fdb21ab7e6
suzuka-fukami <suzuka.fukami@gmail.com>
2021-07-13 11:10:21 +1000

    Installed react-navigation version 4, react-navigation-stack, react-navigation-tabs, and react-navigation-drawer

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,4 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
