af58710fba0463cc14345e185c9170babcc1cf08
suzuka-fukami <suzuka.fukami@gmail.com>
2021-07-14 11:36:09 +1000

    Installed react-navigation-tabs and implemented the basic setup to MealsNavigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,15 +1,16 @@
 import { Platform } from "react-native";
-// MEMO -- react-navigation-stack manages stacks of screens and the according animations required under the hood
-import { createStackNavigator } from "react-navigation-stack";
 // MEMO -- createAppContainer is ALWAYS imported from react-navigation, regardless of which react-navigation version you're using
 import { createAppContainer } from "react-navigation";
-// import { createBottomTabNavigator } from 'react-navigation-tabs';
+// MEMO -- react-navigation-stack manages stacks of screens and the according animations required under the hood
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 // import { createDrawerNavigator } from 'react-navigation-drawer';
 
 import Colors from "../constants/Colors";
 import {
   CategoriesScreen,
   CategoryMealsScreen,
+  FavoritesScreen,
   MealDetailScreen,
 } from "../screens";
 
@@ -29,8 +30,21 @@ const MealsNavigator = createStackNavigator(
         backgroundColor: Platform.OS === "android" ? Colors.primary : "",
       },
       headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+      // MEMO -- by default, on ios, back button includes the label of the previous screen if it fits in available space, otherwise it says "Back"
+      headerBackTitle: "Back",
     },
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen,
+});
+
+// MEMO -- Stack navigator 'MealsNavigator' is now nested inside the Tab Navigator
+/**
+ * |- createAppContainer (react-navigation)
+ * |  |- MealsFabTabNavigator (react-navigation-tabs)
+ * |    |- MealsNavigator (react-navigation-stack)
+ */
+export default createAppContainer(MealsFavTabNavigator);
