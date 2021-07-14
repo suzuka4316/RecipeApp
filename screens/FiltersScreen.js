f4df96436582e18558dd2b4fc38101d9ad34e15c
suzuka-fukami <suzuka.fukami@gmail.com>
2021-07-14 16:52:25 +1000

    Displayed 4 switches for 4 filters

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,13 +1,71 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+// MEMO -- Switch is a toggle switch
+import { Platform, StyleSheet, Switch, Text, View } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import { CategoryGridTile, HeaderButton } from "../components";
+import { HeaderButton } from "../components";
+import Colors from "../constants/Colors";
+
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.primary }}
+        thumbColor={Platform.OS === "android" ? Colors.primary : ""}
+        value={props.state}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+};
 
 export const FiltersScreen = (props) => {
+  // MEMO -- usually on React, if you use any type of user input, you need to use hooks to save its state.
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
+  // MEMO -- useCallback() allows us to wrap a function so the function is cached by React and is only recreated if its dependencies changed.
+  // It's probably a good idea to wrap a function which is a dependency of useEffect(), because function (saveFilters) inside a function (FiltersScreen component and useEffect()) is recreated every time the parent component rerenders. (a component rerenders every time its props or states change.) And that would cause an infinite loop!
+  const saveFilters = useCallback(() => {
+    const appliedFilters = {
+      glutenFree: isGlutenFree,
+      lactoseFree: isLactoseFree,
+      isVegan: isVegan,
+      isVegetarian: isVegetarian,
+    };
+  }, [isGlutenFree, isLactoseFree, isVegetarian, isVegan]);
+
+  // MEMO -- navigation.setParams() allows you to access data inside a component from outside of the component
+  useEffect(() => {
+    navigation.setParams({ save: saveFilters });
+  }, [saveFilters]);
+
   return (
     <View style={styles.screen}>
-      <Text>Filters Screen</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        label="Gluten-free"
+        state={isGlutenFree}
+        onChange={(newValue) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label="Lactose-free"
+        state={isLactoseFree}
+        onChange={(newValue) => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        state={isVegan}
+        onChange={(newValue) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={(newValue) => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -27,13 +85,37 @@ FiltersScreen.navigationOptions = (navData) => {
         />
       </HeaderButtons>
     ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Save"
+          iconName="ios-save"
+          onPress={
+            //get the 4 states data inside FiltersScreen component
+            navData.navigation.getParam("save")
+          }
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginVertical: 15,
   },
 });
